068b23e53b0e3736af2a765c8aea43bd4477fee1
Mbugua <mbuguamark@adalabsafrica.com>
2022-06-22 10:29:52 +0300

    Chore: Add comments to codebase

--- src/Components/Counter/counter.jsx
+++ src/Components/Counter/counter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import MutliplyButton from "../Buttons/multiply-button";
@@ -7,15 +7,13 @@ import CustomForm from "../CustomForm/custom-form";
 // import "../../API/db.json";
 import styles from "./counter.module.scss";
 
-const SampleContext = React.createContext();
-
 const Counter = () => {
   const [counter, setCounter] = useState("Blow");
-  const [calcResult, setCalcResult] = useState(1);
   const [formCounter, setFormCounter] = useState(null);
   const [isEven, setIsEven] = useState(false);
   const [data, setData] = useState(false);
 
+  //No.2: Find out if counter value is even using an open source API
   const isCounterEven = async () => {
     if (counter % 1 !== 0 || counter == "Infinity") {
       setIsEven(false);
@@ -44,6 +42,7 @@ const Counter = () => {
     value: counter,
   };
 
+  //No.3: to change the Counter Value to a random Value.....
   const addRandomCounterValue = async () => {
     try {
       const response = await axios.post(
@@ -63,6 +62,7 @@ const Counter = () => {
     addRandomCounterValue();
   }, [counter]);
 
+  //No.5: press the enter button after to submit the form input vale
   useEffect(() => {
     const keyDownHandler = (e) => {
       console.log("User pressed: ", e.key);
@@ -77,20 +77,24 @@ const Counter = () => {
 
     document.addEventListener("keydown", keyDownHandler);
 
+    //Clean up fn when component unmounts
     return () => {
       document.removeEventListener("keydown", keyDownHandler);
     };
-  }, [counter]);
+  }, [formCounter]);
 
+  //No.1(a) First Button (Multiply) must multiply the value (2^2, 2^3, etc.)
   const handleMultiplication = () => {
     setCounter(Math.pow(counter, counter));
   };
 
+  //No.1(b) Second Button (Square) must square the value (sqrt(2^3) for example)
   const handleSquareRoot = () => {
     setCounter(Math.sqrt(counter));
   };
 
   return (
+    // No.4: Hide & disable the Buttons, if the Value is not a Number......
     <>
       <div className={styles.counterContainer}>
         {isNaN(counter) === false && counter !== null ? (
@@ -102,16 +106,12 @@ const Counter = () => {
               setCounter={setCounter}
               handleSubmit={handleSubmit}
               setFormCounter={setFormCounter}
-              calcResult={calcResult}
-              SampleContext={SampleContext}
             />
             <div className={styles.counterDetails}>
               <h3>Counter Value: {counter}</h3>
               <br />
-              <h5>
-                Is counter value an even number?{" "}
-                {isEven ? <h4>Yes</h4> : <h4>No</h4>}
-              </h5>
+              <h5>Is counter value an even number? </h5>
+              {isEven ? <h4>Yes</h4> : <h4>No</h4>}
               <br />
             </div>
             <SquareButton
@@ -121,7 +121,6 @@ const Counter = () => {
               handleSubmit={handleSubmit}
               formCounter={formCounter}
               setFormCounter={setFormCounter}
-              SampleContext={SampleContext}
             />
           </div>
         ) : (

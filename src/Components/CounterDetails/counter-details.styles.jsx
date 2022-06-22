eb4166eebfa7aa397e7eb46e8f5d92d6ba4b1f08
Mbugua <mbuguamark@adalabsafrica.com>
2022-06-22 14:30:12 +0300

    Chore: Refactoring session, sprint 3

---
+++ src/Components/CounterDetails/counter-details.styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const CounterDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  padding: 20px;
+  border: 1px solid grey;
+  border-radius: 1.2rem;
+`;
+
+export const IsEvenContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const IsEvenBoolean = styled.p`
+  font-weight: 470;
+  color: ${({ isEven }) => (isEven === true ? "#24c918" : "#9c00f6")};
+`;
